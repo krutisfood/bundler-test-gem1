546737f1265bb37db97a6ecc4f259b5840cccafa
Kurt Gardiner <k.a.gardiner@gmail.com>
2017-07-11 16:43:14 +1000

    :flag-au: Adds electricity consumer

--- lib/bundler-test-gem1.rb
+++ lib/bundler-test-gem1.rb
@@ -1,5 +1,12 @@
 require 'bundler-test-gem1/does-things'
+require 'utilities'
 
 module BundlerTestGem1
   VERSION = '0.0.1'
+
+  class BitCoin
+    def self.mine
+      Utilities::Electricity.consume
+    end
+  end
 end
---
+++ lib/utilities.rb
@@ -0,0 +1,7 @@
+module Utilities
+  class Electricity
+    def self.consume
+      puts "Hi ho, hi ho, it's mining bitcoins we go..."
+    end
+  end
+end
